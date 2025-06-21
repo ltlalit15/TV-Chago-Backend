ac0a4235ee7f2d301879528e75fe68de33bb3555
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-06-21 11:43:49 +0530

    d

--- Models/ServiceModel.js
+++ Models/ServiceModel.js
@@ -4,15 +4,15 @@ const serviceSchema = new mongoose.Schema(
   {
     serviceName: {
       type: String,
-      required: true,
+
     },
     price: {
       type: String,
-      required: true,
+
     },
     duration: {
       type: String,
-      required: true,
+
     },
 
   },
