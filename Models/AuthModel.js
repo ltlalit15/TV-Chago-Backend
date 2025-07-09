a7dbc3e90384b2dbf211e5c0858a6cb8b3814552
Mohammad Rehan Multani <135141111+Rehan-Multani@users.noreply.github.com>
2025-07-09 12:37:17 +0530

    d

--- Models/AuthModel.js
+++ Models/AuthModel.js
@@ -28,18 +28,6 @@ const authSchema = new mongoose.Schema(
     }
 );
 
-// Hash password before saving
-authSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
-
-// Method to compare password
-authSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const Auth = mongoose.model("User", authSchema);
 
